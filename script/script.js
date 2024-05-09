c74b1ce70e3676e16f8ba2fda62c04bc929e11d2
Jorge Luiz <j87150712@gmail.com>
2024-05-09 14:30:47 -0300

    atualização

---
+++ script/script.js
@@ -0,0 +1,337 @@
+
+function enviar() {
+    let camp = document.getElementById('person').value
+    let res = document.getElementById('res')
+    let selecione = 'selecione'
+    let alastor = 'alastor'
+    let alvo ="alvo"
+    let argo ="argo"
+    let arthur ="arthur"
+    let bartor ="bartor"
+    let bartorjr ="bartorjr"
+    let belatrix ="belatrix"
+    let carlinhos ="carlinhos"
+    let cedrico ="cedrico"
+    let cho ="cho"
+    let colin ="colin"
+    let Crabbe ="Crabbe"
+    let dino ="dino"
+    let doby ="doby"
+    let dolores ="dolores"
+    let draco ="draco"
+    let duda ="duda"
+    let ernesto ="ernesto"
+    let fenrir ="fenrir"
+    let filius ="filius"
+    let fleur ="fleur"
+    let flitwink ="flitwink"
+    let fred ="fred"
+    let fudge ="fudge"
+    let gilderoy ="gilderoy"
+    let gina ="gina"
+    let goily ="goily"
+    let grampo ="grampo"
+    let grope ="grope"
+    let guida ="guida"
+    let hagrid ="hagrid"
+    let harry ="harry"
+    let hermione ="hermione"
+    let horacio ="horacio"
+    let jorge ="jorge"
+    let lila ="lila"
+    let lilian ="lilian"
+    let lucio ="lucio"
+    let luna ="luna"
+    let lupin ="lupin"
+    let minerva ="minerva"
+    let molly ="molly"
+    let murta ="murta"
+    let narcisa ="narcisa"
+    let neville ="neville"
+    let olivaras ="olivaras"
+    let percy ="percy"
+    let parvati ="parvati"
+    let petunia ="petunia"
+    let quirell ="quirell"
+    let rabicho ="rabicho"
+    let rone ="rone"
+    let sibila ="sibila"
+    let simas ="simas"
+    let sirius ="sirius"
+    let snape ="snape"
+    let sprout ="sprout"
+    let stanislau ="stanislau"
+    let thiago ="thiago"
+    let tom ="tom"
+    let tonks ="tonks"
+    let voldemort ="voldemort"
+    let valter ="valter"
+    let vitor ="vitor"
+    let wood ="wood"
+   
+    if(camp === selecione) {
+        res.innerHTML = ''
+        info.innerHTML = ''              
+    }
+
+    if(camp === alastor) {
+        res.innerHTML = `<img src="imagens/alastor300.png" alt=""> `
+        info.innerHTML = `<p>Nome: Alastor Moody <br>  </p>`                 
+    }
+
+    if(camp === alvo) {
+        res.innerHTML = `<img src="imagens/alvo300.png" alt="">`                 
+    }
+
+    if(camp === argo) {
+        res.innerHTML = `<img src="imagens/argo300.png" alt="">`                 
+    }
+
+    if(camp === arthur) {
+        res.innerHTML = `<img src="imagens/arthur300.png" alt="">`                 
+    }
+
+    if(camp === bartor) {
+        res.innerHTML = `<img src="imagens/bartor300.png" alt="">`                 
+    }
+
+    if(camp === bartorjr) {
+        res.innerHTML = `<img src="imagens/bartorjr.png" alt="">`                 
+    }
+
+    if(camp === belatrix) {
+        res.innerHTML = `<img src="imagens/belatrix300.png" alt="">`                 
+    }
+
+    if(camp === carlinhos) {
+        res.innerHTML = `<img src="imagens/carlinho300.png" alt="">`                 
+    }
+
+    if(camp === cedrico) {
+        res.innerHTML = `<img src="imagens/cedrico300.png" alt="">`                 
+    }
+
+    if(camp === cho) {
+        res.innerHTML = `<img src="imagens/cho300.png" alt="">`                 
+    }
+
+    if(camp === colin) {
+        res.innerHTML = `<img src="imagens/colin300.png" alt="">`                 
+    }
+
+    if(camp === Crabbe) {
+        res.innerHTML = `<img src="imagens/crab300.png" alt="">`                 
+    }
+
+    if(camp === dino) {
+        res.innerHTML = `<img src="imagens/dino300.png" alt="">`                 
+    }
+
+    if(camp === doby) {
+        res.innerHTML = `<img src="imagens/doby300.png" alt="">`                 
+    }
+
+    if(camp === dolores) {
+        res.innerHTML = `<img src="imagens/dolores.png" alt="">`                 
+    }
+
+    if(camp === draco) {
+        res.innerHTML = `<img src="imagens/draco300.png" alt="">`                 
+    }
+
+    if(camp === duda) {
+        res.innerHTML = `<img src="imagens/duda300.png" alt="">`                 
+    }
+
+    if(camp === ernesto) {
+        res.innerHTML = `<img src="imagens/ernesto300.png" alt="">`                 
+    }
+
+    if(camp === fenrir) {
+        res.innerHTML = `<img src="imagens/fenrir300.png" alt="">`                 
+    }
+
+    if(camp === filius) {
+        res.innerHTML = `<img src="imagens/filius.png" alt="">`                 
+    }
+
+    if(camp === fleur) {
+        res.innerHTML = `<img src="imagens/fleur300.png" alt="">`                 
+    }
+
+    if(camp === flitwink) {
+        res.innerHTML = `<img src="imagens/flitwink300.png" alt="">`                 
+    }
+
+    if(camp === fred) {
+        res.innerHTML = `<img src="imagens/fred300.png" alt="">`                 
+    }
+
+    if(camp === fudge) {
+        res.innerHTML = `<img src="imagens/fudge300.png" alt="">`                 
+    }
+
+    if(camp === gilderoy) {
+        res.innerHTML = `<img src="imagens/gilderoy300.png" alt="">`                 
+    }
+
+    if(camp === gina) {
+        res.innerHTML = `<img src="imagens/gina300.png" alt="">`                 
+    }
+
+    if(camp === goily) {
+        res.innerHTML = `<img src="imagens/goily300.png" alt="">`                 
+    }
+
+    if(camp === grampo) {
+        res.innerHTML = `<img src="imagens/grampo300.png" alt="">`                 
+    }
+
+    if(camp === grope) {
+        res.innerHTML = `<img src="imagens/grop300.png" alt="">`                 
+    }
+
+    if(camp === guida) {
+        res.innerHTML = `<img src="imagens/guida300.png" alt="">`                 
+    }
+
+    if(camp === hagrid) {
+        res.innerHTML = `<img src="imagens/hagrid300.png" alt="">`                 
+    }
+
+    if(camp === harry) {
+        res.innerHTML = `<img src="imagens/harry300.png" alt="">`                 
+    }
+
+    if(camp === hermione) {
+        res.innerHTML = `<img src="imagens/hemione300.png" alt="">`                 
+    }
+
+    if(camp === horacio) {
+        res.innerHTML = `<img src="imagens/horacio300.png" alt="">`                 
+    }
+
+    if(camp === jorge) {
+        res.innerHTML = `<img src="imagens/jorge300.png" alt="">`                 
+    }
+
+    if(camp === lila) {
+        res.innerHTML = `<img src="imagens/lila300.png" alt="">`                 
+    }
+
+    if(camp === lilian) {
+        res.innerHTML = `<img src="imagens/lilian300.png" alt="">`                 
+    }
+
+    if(camp === lucio) {
+        res.innerHTML = `<img src="imagens/lucio300.png" alt="">`                 
+    }
+
+    if(camp === luna) {
+        res.innerHTML = `<img src="imagens/luna300.png" alt="">`                 
+    }
+
+    if(camp === lupin) {
+        res.innerHTML = `<img src="imagens/lupin300.png" alt="">`                 
+    }
+
+    if(camp === minerva) {
+        res.innerHTML = `<img src="imagens/minerva300.png" alt="">`                 
+    }
+
+    if(camp === molly) {
+        res.innerHTML = `<img src="imagens/moly300.png" alt="">`                 
+    }
+
+    if(camp === murta) {
+        res.innerHTML = `<img src="imagens/murta300.png" alt="">`                 
+    }
+
+    if(camp === narcisa) {
+        res.innerHTML = `<img src="imagens/narcisa300.png" alt="">`                 
+    }
+
+    if(camp === neville) {
+        res.innerHTML = `<img src="imagens/neville300.png" alt="">`                 
+    }
+
+    if(camp === olivaras) {
+        res.innerHTML = `<img src="imagens/olivaras300.png" alt="">`                 
+    }
+
+    if(camp === percy) {
+        res.innerHTML = `<img src="imagens/parcy300.png" alt="">`                 
+    }
+
+    if(camp === parvati) {
+        res.innerHTML = `<img src="imagens/parvati300.png" alt="">`                 
+    }
+
+    if(camp === petunia) {
+        res.innerHTML = `<img src="imagens/petunia300.png" alt="">`                 
+    }
+
+    if(camp === quirell) {
+        res.innerHTML = `<img src="imagens/querill300.png" alt="">`                 
+    }
+
+    if(camp === rabicho) {
+        res.innerHTML = `<img src="imagens/rabicho300.png" alt="">`                 
+    }
+
+    if(camp === rone) {
+        res.innerHTML = `<img src="imagens/rone300.png" alt="">`                 
+    }
+
+    if(camp === sibila) {
+        res.innerHTML = `<img src="imagens/sibila300.png" alt="">`                 
+    }
+
+    if(camp === simas) {
+        res.innerHTML = `<img src="imagens/simas300.png" alt="">`                 
+    }
+
+    if(camp === sirius) {
+        res.innerHTML = `<img src="imagens/sirius300.png" alt="">`                 
+    }
+
+    if(camp === snape) {
+        res.innerHTML = `<img src="imagens/snape300.png" alt="">`                 
+    }
+
+    if(camp === sprout) {
+        res.innerHTML = `<img src="imagens/sprout300.png" alt="">`                 
+    }
+
+    if(camp === stanislau) {
+        res.innerHTML = `<img src="imagens/stanilau300.png" alt="">`                 
+    }
+
+    if(camp === thiago) {
+        res.innerHTML = `<img src="imagens/thiago300.png" alt="">`                 
+    }
+
+    if(camp === tom) {
+        res.innerHTML = `<img src="imagens/tom300.png" alt="">`                 
+    }
+
+    if(camp === tonks) {
+        res.innerHTML = `<img src="imagens/tonks300.png" alt="">`                 
+    }
+
+    if(camp === voldemort) {
+        res.innerHTML = `<img src="imagens/valdemort300.png" alt="">`                 
+    }
+
+    if(camp === valter) {
+        res.innerHTML = `<img src="imagens/valter_dursley.png" alt="">`                 
+    }
+
+    if(camp === vitor) {
+        res.innerHTML = `<img src="imagens/vitor300.png" alt="">`                 
+    }
+
+    if(camp === wood) {
+        res.innerHTML = `<img src="imagens/wood300.png" alt="">`                 
+    }
+}
